931bcc519c72eaa8a4d23600e3dd9e3f6b932429
Diego Silveira <diegocilveira@gmail.com>
2025-04-09 22:09:12 -0300

    inicio

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* Jogo da força criado como atividade na faculdade durante aprendizado da linguagem C++
+   Criado por: Diego Cristino Silveira
+   Data: 09/04/2025 */
+
+#include <iostream>
+#include <iomanip>
+
+
+main(){
+    system("chcp 65001");
+    system(cls);
+}
